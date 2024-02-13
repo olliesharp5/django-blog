69e7362d4b1ed7b939cd90076eb24505cb0b8e28
Oliver Sharp <olliesharp5@gmail.com>
2024-02-13 15:53:39 +0000

    Added summernote to enhance admin dashboard

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-olliesharp5-djangoblog-sfrydjasmx6.ws-eu108.gitpod.io', 
                  '.herokuapp.com']
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
