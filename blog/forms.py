09ec9f66b82244e10c3e4e0b6f8e0059c8b25397
Oliver Sharp <olliesharp5@gmail.com>
2024-02-18 13:02:51 +0000

    Added content from templates

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class CollaborateForm(forms.ModelForm):
+    class Meta:
+        model = CollaborateRequest
+        exclude = ['read']
