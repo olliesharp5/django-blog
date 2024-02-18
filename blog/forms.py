ae16d574b7521c29e2fba4852a32582aab115863
Oliver Sharp <olliesharp5@gmail.com>
2024-02-18 13:14:17 +0000

    Added collaboration form to about page

--- blog/forms.py
+++ blog/forms.py
@@ -6,8 +6,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
-
-class CollaborateForm(forms.ModelForm):
-    class Meta:
-        model = CollaborateRequest
-        exclude = ['read']
